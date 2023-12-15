e6e178277d036a989f1f6954f46e1670dee4bb34
BT_1N3_06 <example@bths.edu>
2023-12-15 10:36:46 -0500

    added print statement

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,6 @@
-public class runner {
+public class Runner {
     public static void main(String[] args){
         System.out.println("kill everyone with long knives");
+        System.out.println("I will help you!");
     }
 }
