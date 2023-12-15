7a713c1011b5135f2ceb4ea0fb93324efbbad217
BT_1N3_06 <example@bths.edu>
2023-12-15 10:55:22 -0500

    urMom() method

--- src/Cringe2.java
+++ src/Cringe2.java
@@ -3,4 +3,8 @@ public class Cringe2 {
     public static void print(){
         System.out.println("69 + 420");
     }
+    public static void urMom() {
+        System.out.println("ur mommas so");
+    }
+
 }
