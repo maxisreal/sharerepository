6502735fca993088256b138347275d58cd882b47
BT_1N3_05 <maximillianw6@nycstudents.net>
2023-12-15 10:37:56 -0500

    added thing

--- src/Runner.java
+++ src/Runner.java
@@ -2,5 +2,6 @@ public class Runner {
     public static void main(String[] args){
         System.out.println("kill everyone with long knives");
         System.out.println("I will help you!");
+        System.out.println("sssssssssssssssssssssssssss");
     }
 }
