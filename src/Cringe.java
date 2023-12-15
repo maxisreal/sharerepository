9d44ce673b54c97284d17aae4164db20e0ca1730
BT_1N3_05 <maximillianw6@nycstudents.net>
2023-12-15 10:45:30 -0500

    added more more cringe

--- src/Cringe.java
+++
@@ -1,9 +0,0 @@
-public class Cringe {
-    private Cringe(){}
-    public static void print(){
-        System.out.println("lambda");
-    }
-    public static void urMom(){
-        System.out.println("your mommas so ");
-    }
-}
